e3a66e8ad08c5de2458f6f4bfbf6d9c95cc9d4a4
ric-04 <wallace.l.ric@gmail.com>
2021-08-03 06:16:58 -0400

    Add project files.

---
+++ GeneralStore.MVC/Models/Transaction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId {get; set; }
+        [ForeignKey(nameof(Customer))]
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+        [ForeignKey(nameof(Product))]
+        public virtual Product Product { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalCost
+        {
+            get
+            {
+                return Product.UnitPrice * Quantity;
+            }
+        }
+    }
+}
